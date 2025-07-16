94007e17dbf5c141ced105f39ddc00ba2ea5bfaf
luke <luke@asgard>
2025-07-16 15:44:15 -0500

    Show ignore_immutable status on header

--- src/view.rs
+++ src/view.rs
@@ -11,6 +11,28 @@ use ratatui::{
 };
 
 pub fn view(model: &mut Model, frame: &mut Frame) {
+    let mut header_spans = vec![
+        Span::styled("repository: ", Style::default().fg(Color::Blue)),
+        Span::styled(
+            &model.global_args.repository,
+            Style::default().fg(Color::Green),
+        ),
+        Span::raw("  "),
+        Span::styled("revset: ", Style::default().fg(Color::Blue)),
+        Span::styled(&model.revset, Style::default().fg(Color::Green)),
+    ];
+    if model.global_args.ignore_immutable {
+        header_spans.push(Span::styled(
+            "  --ignore-immutable",
+            Style::default().fg(Color::Red),
+        ));
+    }
+    let header = Paragraph::new(Line::from(header_spans));
+
+    let log_list = List::new(model.log_list.clone())
+        .highlight_style(Style::new().bold().bg(Color::from_str("#282A36").unwrap()))
+        .scroll_padding(model.log_list_scroll_padding);
+
     let layout = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
@@ -23,21 +45,6 @@ pub fn view(model: &mut Model, frame: &mut Frame) {
             },
         ])
         .split(frame.area());
-
-    let header = Paragraph::new(Line::from(vec![
-        Span::styled("repository: ", Style::default().fg(Color::Blue)),
-        Span::styled(
-            &model.global_args.repository,
-            Style::default().fg(Color::Green),
-        ),
-        Span::raw("   "),
-        Span::styled("revset: ", Style::default().fg(Color::Blue)),
-        Span::styled(&model.revset, Style::default().fg(Color::Green)),
-    ]));
-
-    let log_list = List::new(model.log_list.clone())
-        .highlight_style(Style::new().bold().bg(Color::from_str("#282A36").unwrap()))
-        .scroll_padding(model.log_list_scroll_padding);
 
     frame.render_widget(header, layout[0]);
     frame.render_stateful_widget(log_list, layout[1], &mut model.log_list_state);
